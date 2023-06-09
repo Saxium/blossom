3318b6ca1e3121a18e9589ccb0bdae623813d18e
John Hetherington <john.hetherington@havonix.co.uk>
2023-06-09 07:38:04 +0100

    Add typing

--- blossom.py
+++ blossom.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 
 import sys
-import argparse
+from argparse import ArgumentParser
 import os.path
-import json
 import logging
-
-from typing import List, TextIO, Optional
+from logging import Logger
 
 class Blossom:
     """Blossom Online Word Game"""
-    TMP_FILE = '/tmp/blossom.txt'
+    TMP_FILE:str = '/tmp/blossom.txt'
 
-    def __init__(self, parser, logger = None) -> None:
+    def __init__(self, parser: ArgumentParser, logger: Logger = None) -> None:
         self.parser = parser
         self.logger = logger or logging.getLogger(__name__)
 
     def _match(self, word: str, pistil, petals) -> bool:
+        """Match words with the pistil and optional number of petals """
         if word.find(pistil) >= 0:
-            x = set(word) - set(petals)
-            y = set(pistil)
-            if x == y:
+            if set(word) - set(petals) == set(pistil):
                 return True
         return False
 
-    def _filter_words(self, words_name, min, pistil, petals):
-        words = []
+    def _filter_words(self, words_name, min_length, pistil, petals) -> tuple:
+        """Load words and filter"""
+        words: list = []
         if not os.path.exists(words_name):
             self.parser.error(f'No such file: {words_name}')
             return (False, None)
@@ -37,15 +35,16 @@ class Blossom:
 
         for line in words_file:
             word = line.strip()
-            if len(word) >= min:
+            if len(word) >= min_length:
                 if self._match(word, pistil, petals):
                     words.append(word)
 
         words_file.close()
         return (True, words)
 
-    def _length_bonus(self, word):
-        length = len(word)
+    def _length_bonus(self, word) -> int:
+        """Formula for word length bonus"""
+        length: int = len(word)
         if length < 4:
             return 0
         if length == 4:
@@ -56,48 +55,51 @@ class Blossom:
             return 6
         return 12 + (length - 7) * 3
 
-    def _rank(self, words: list, bonus: str, score: int):
-        build = {}
+    def _rank(self, words: list, bonus: str, min_score: int) -> dict:
+        """Calc word and total score"""
+        build: dict = {}
         for word in words:
-            bonus_chars = [item for item in list(word) if item == bonus]
-            other_chars = [item for item in list(word) if item != bonus]
-            all_bonus = 0
+            bonus_chars: list = [item for item in list(word) if item == bonus]
+            all_bonus: int = 0
             if len(set(word)) == 7:
                 all_bonus = 7
-            rank = self._length_bonus(word) + len(bonus_chars)*5 + all_bonus
-            if rank >= score:
-                build[word] = rank
+            score: int = self._length_bonus(word) + len(bonus_chars)*5 + all_bonus
+            if score >= min_score:
+                build[word] = score
         return sorted((value,key) for (key,value) in build.items())
 
-    def doit(self, words_name: str, flower: str, bonus: str = None, min: int = 1, score: int = 1) -> bool:
-        """Create temp file with flower only"""
+    def bloom(self, words_name: str, flower: str, bonus: str = None, min_length: int = 1, score: int = 1) -> bool:
+        """Main blossum driver"""
         if len(set(flower)) != 7:
-            self.parser.error(f'Seven unique chars required for flower')
+            self.parser.error('Seven unique chars required for flower')
             return False
 
         if len(bonus) != 1:
-            self.parser.error(f'Single char required for bonus')
+            self.parser.error('Single char required for bonus')
             return False
 
-        petals = list(flower)
-        pistil = petals.pop(0)
+        if len(set(flower + bonus)) != 7:
+            self.parser.error(f'Bonus "{bonus}" must be in flower "{flower}"')
+            return False
 
-        status, word_list = self._filter_words(words_name, min, pistil, petals)
+        petals: list = list(flower)
+        pistil: str = petals.pop(0)
+
+        status, word_list = self._filter_words(words_name, min_length, pistil, petals)
         if status is False:
             return False
 
         ranks = self._rank(word_list, bonus, score)
 
-        for x in ranks:
-            rank, word = x
+        for _ in ranks:
+            rank, word = _
             print(f'{rank} : {word}')
 
         return True
 
-
 def main() -> bool:
     """Main to handle program parameters"""
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument('-w', '--words', default="words_alpha.txt", help="alpha words")
     parser.add_argument('-l', '--log', help="logging output", action='store_true')
     parser.add_argument('-f', '--flower', required=True, help="petals (pistil first)")
@@ -108,29 +110,24 @@ def main() -> bool:
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
-    ch = logging.StreamHandler()
+    handle = logging.StreamHandler()
 
     if args.log:
         logger.setLevel(logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
+        handle.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
-        ch.setLevel(logging.INFO)
+        handle.setLevel(logging.INFO)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    handle.setFormatter(formatter)
+    logger.addHandler(handle)
 
     blossom: Blossom = Blossom(parser, logger)
 
-    if args.flower:
-        return blossom.doit(args.words, args.flower, args.bonus, args.min, args.score)
-
-    return False
+    return blossom.bloom(args.words, args.flower, args.bonus, args.min, args.score)
 
 if __name__ == "__main__":
     if main():
-        print("OK")
         sys.exit(0)
-    print("ERR")
     sys.exit(1)

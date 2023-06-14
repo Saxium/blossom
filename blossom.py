7d2e03ee5183e3e0c51de3b15b5d8629998b3d7e
John Hetherington <john.hetherington@havonix.co.uk>
2023-06-14 11:28:22 +0100

    Restructure. dos2unix on alpha

--- blossom.py
+++ blossom.py
@@ -5,14 +5,16 @@ from argparse import ArgumentParser
 import os.path
 import logging
 from logging import Logger
+from typing import Optional
 
 class Blossom:
     """Blossom Online Word Game"""
-    TMP_FILE:str = '/tmp/blossom.txt'
 
-    def __init__(self, parser: ArgumentParser, logger: Logger = None) -> None:
+    def __init__(self, parser: ArgumentParser,logger: Optional[Logger]) -> None:
         self.parser = parser
         self.logger = logger or logging.getLogger(__name__)
+        self._words = None
+        self._ranks = None
 
     def _match(self, word: str, pistil, petals) -> bool:
         """Match words with the pistil and optional number of petals """
@@ -55,32 +57,8 @@ class Blossom:
             return 6
         return 12 + (length - 7) * 3
 
-    def _rank(self, words: list, bonus: str, min_score: int) -> dict:
-        """Calc word and total score"""
-        build: dict = {}
-        for word in words:
-            bonus_chars: list = [item for item in list(word) if item == bonus]
-            all_bonus: int = 0
-            if len(set(word)) == 7:
-                all_bonus = 7
-            score: int = self._length_bonus(word) + len(bonus_chars)*5 + all_bonus
-            if score >= min_score:
-                build[word] = score
-        return sorted((value,key) for (key,value) in build.items())
-
-    def bloom(self, words_name: str, flower: str, bonus: str = None, min_length: int = 1, score: int = 1) -> bool:
+    def load(self, words_name: str, flower: str, min_length: int = 1) -> bool:
         """Main blossum driver"""
-        if len(set(flower)) != 7:
-            self.parser.error('Seven unique chars required for flower')
-            return False
-
-        if len(bonus) != 1:
-            self.parser.error('Single char required for bonus')
-            return False
-
-        if len(set(flower + bonus)) != 7:
-            self.parser.error(f'Bonus "{bonus}" must be in flower "{flower}"')
-            return False
 
         petals: list = list(flower)
         pistil: str = petals.pop(0)
@@ -89,18 +67,37 @@ class Blossom:
         if status is False:
             return False
 
-        ranks = self._rank(word_list, bonus, score)
+        self._words = word_list
+        return True
 
-        for _ in ranks:
+    def rank(self, bonus: str, min_score: int = 1) -> None:
+        """Calc word and total score"""
+        assert self._words, "Words list should exist"
+        build: dict = {}
+        for word in self._words:
+            bonus_chars: list = [item for item in list(word) if item == bonus]
+            all_bonus: int = 0
+            if len(set(word)) == 7:
+                all_bonus = 7
+            score: int = self._length_bonus(word) + len(bonus_chars)*5 + all_bonus
+            if score >= min_score:
+                build[word] = score
+        self._ranks = sorted((value,key) for (key,value) in build.items())
+
+    def show(self) -> None:
+        """Show results"""
+        assert self._ranks, "Ranks list should exist"
+        for _ in self._ranks:
             rank, word = _
             print(f'{rank} : {word}')
 
-        return True
+
+
 
 def main() -> bool:
     """Main to handle program parameters"""
     parser = ArgumentParser()
-    parser.add_argument('-w', '--words', default="words_alpha.txt", help="alpha words")
+    parser.add_argument('-w', '--words', default="words.txt", help="alpha words")
     parser.add_argument('-l', '--log', help="logging output", action='store_true')
     parser.add_argument('-f', '--flower', required=True, help="petals (pistil first)")
     parser.add_argument('-b', '--bonus', required=True, help="bonus letter")
@@ -123,9 +120,26 @@ def main() -> bool:
     handle.setFormatter(formatter)
     logger.addHandler(handle)
 
+    if len(set(args.flower)) != 7:
+        parser.error('Seven unique chars required for flower')
+        return False
+
+    if len(args.bonus) != 1:
+        parser.error('Single char required for bonus')
+        return False
+
+    if len(set(args.flower + args.bonus)) != 7:
+        parser.error(f'Bonus "{args.bonus}" must be in flower "{args.flower}"')
+        return False
+
     blossom: Blossom = Blossom(parser, logger)
 
-    return blossom.bloom(args.words, args.flower, args.bonus, args.min, args.score)
+    if blossom.load(args.words, args.flower, args.min):
+        blossom.rank(args.bonus, args.min)
+        blossom.show()
+        return True
+
+    return False
 
 if __name__ == "__main__":
     if main():

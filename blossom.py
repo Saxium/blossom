4adb1ed96fcd6ded9bef8ddfc7d1f2e1cd325566
John Hetherington <john.hetherington@havonix.co.uk>
2023-06-19 06:58:45 +0100

    Add --print option

--- blossom.py
+++ blossom.py
@@ -59,11 +59,8 @@ class Blossom:
             return 6
         return 12 + (length - 7) * 3
 
-    def load(self, words_name: str, flower: str, min_length: int = 1) -> bool:
-        """Main blossum driver"""
-
-        petals: list = list(flower)
-        pistil: str = petals.pop(0)
+    def load(self, words_name: str, pistil: str, petals: str, min_length: int = 1) -> bool:
+        """Load words from file"""
 
         status, word_list = self._filter_words(words_name, min_length, pistil, petals)
         if status is False:
@@ -84,26 +81,54 @@ class Blossom:
             score: int = self._length_bonus(word) + len(bonus_chars) * 5 + all_bonus
             if score >= min_score:
                 build[word] = score
-        self._ranks = sorted((value, key) for (key, value) in build.items())
+        return sorted((value, key) for (key, value) in build.items())
 
-    def show(self) -> None:
+    def _avg_top(self, petals: str, min_score: int = 1):
+        """Top twenty averaged"""
+        build = {}
+        for bonus in petals:
+            ranks = self.rank(bonus, min_score)
+            ranks_count = len(ranks)
+            if ranks_count > 20:
+                ranks_count = 20
+            build[bonus] = ranks[:ranks_count]
+
+    def calc_top(self, petals: str, min_score: int = 1):
+        """Calculate top score"""
+        pass
+
+    def simple_print(self, ranks) -> None:
         """Show results"""
-        assert self._ranks, "Ranks list should exist"
-        for _ in self._ranks:
+        assert ranks, "Ranks list should exist"
+        for word in self._words:
+            print(f'{word}')
+
+    def show(self, ranks) -> None:
+        """Show results"""
+        assert ranks, "Ranks list should exist"
+        for _ in ranks:
             rank, word = _
             print(f'{rank} : {word}')
 
-
-def main() -> bool:
-    """Main to handle program parameters"""
+def blossom_parser():
+    """Blossom parser"""
     parser = ArgumentParser()
     parser.add_argument('-w', '--words', default="words.txt", help="alpha words")
     parser.add_argument('-l', '--log', help="logging output", action='store_true')
     parser.add_argument('-f', '--flower', required=True, help="petals (pistil first)")
-    parser.add_argument('-b', '--bonus', required=True, help="bonus letter")
     parser.add_argument('-m', '--min', type=int, default=1, help="minium word length")
     parser.add_argument('-s', '--score', type=int, default=1, help="words above score")
 
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-b', '--bonus', help="bonus letter")
+    group.add_argument('-t', '--top', action="store_true", default=False, help="highest solution")
+    group.add_argument('-p', '--print', action="store_true", default=False, help="just print")
+
+    return parser
+
+def main() -> bool:
+    """Main to handle program parameters"""
+    parser = blossom_parser()
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
@@ -124,20 +149,29 @@ def main() -> bool:
         parser.error('Seven unique chars required for flower')
         return False
 
-    if len(args.bonus) != 1:
-        parser.error('Single char required for bonus')
-        return False
+    if args.bonus:
+        if len(args.bonus) != 1:
+            parser.error('Single char required for bonus')
+            return False
 
-    if len(set(args.flower + args.bonus)) != 7:
-        parser.error(f'Bonus "{args.bonus}" must be in flower "{args.flower}"')
-        return False
+        if len(set(args.flower + args.bonus)) != 7:
+            parser.error(f'Bonus "{args.bonus}" must be in flower "{args.flower}"')
+            return False
+
+    petals: list = list(args.flower)
+    pistil: str = petals.pop(0)
 
     blossom: Blossom = Blossom(parser, logger)
 
-    if blossom.load(args.words, args.flower, args.min):
-        blossom.rank(args.bonus, args.min)
-        blossom.show()
-        return True
+    if blossom.load(args.words, pistil, petals, args.min):
+        if args.bonus:
+            ranks = blossom.rank(args.bonus, args.score)
+            blossom.show(ranks)
+            return True
+        if args.top:
+            return blossom.calc_top(petals, args.score)
+        if args.print:
+            return blossom.simple_print(petals)
 
     return False
 
